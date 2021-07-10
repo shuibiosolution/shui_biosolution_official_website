7db8348fe8b508bf2d2b303bb5e9739b8745ac18
Li Fengyu <fengyuli2002@gmail.com>
2021-07-10 17:49:29 +0800

    Refine member.jsx.

--- src/about.jsx
+++ src/about.jsx
@@ -6,9 +6,54 @@ import { Layout, Row, Col } from 'antd';
 import SHUIMenu from "./menu.jsx";
 import SHUICard from "./card.jsx";
 import SHUIColorCard from "./colorCard.jsx";
-
+import SHUIMember from "./member.jsx";
 
 const { Header, Content, Footer } = Layout;
+
+// Enter details about members here
+const members = [
+    {
+        name_title: "徐明阳 - CEO",
+        image: "member1.png",
+        bio: "placeholder",
+        linkedin: "https://www.google.com"
+    },
+    {
+        name_title: "高宇轩 - CFO",
+        image: "member2.png",
+        bio: "placeholder",
+        linkedin: "https://www.google.com"
+    },
+    {
+        name_title: "倪赫远 - CTO & Director of R&D",
+        image: "member3.png",
+        bio: "placeholder",
+        linkedin: "https://www.google.com"
+    }
+];
+
+// And details about sponsors
+
+const sponsors = [
+    {
+        name: "徐明阳 - CEO",
+        image: "sponsor1.png",
+        intro1: "placeholder",
+        intro2: "https://www.google.com"
+    },
+    {
+        name: "高宇轩 - CFO",
+        image: "sponsor2.png",
+        intro1: "placeholder",
+        intro2: "https://www.google.com"
+    },
+    {
+        name: "倪赫远 - CTO & Director of R&D",
+        image: "member3.png",
+        intro1: "placeholder",
+        intro2: "https://www.google.com"
+    }
+];
 
 ReactDOM.render(<Layout className="layout">
 
@@ -21,12 +66,16 @@ ReactDOM.render(<Layout className="layout">
         <SHUICard image="/title6.png">
             <div><img src="/title4.png" className="title-text" /></div>
         </SHUICard>
-        <SHUIColorCard style={{ backgroundColor: "#E5E5E5" }}>
+        <SHUIColorCard style={{ backgroundColor: "#2D75EC" }}>
             <div id="card1">
                 <p className="big-text">我们的团队</p>
             </div>
+            {/* Could use a map function or a for-loop here but I forgot how */}
+            <SHUIMember name_title={members[0].name_title} image={members[0].image} bio={members[0].bio} linkedin={members[0].linkedin} />
+            <SHUIMember name_title={members[1].name_title} image={members[1].image} bio={members[1].bio} linkedin={members[1].linkedin} />
+            <SHUIMember name_title={members[2].name_title} image={members[2].image} bio={members[2].bio} linkedin={members[2].linkedin} />
         </SHUIColorCard>
-        <SHUIColorCard style={{ backgroundColor: "#E5E5E5" }}>
+        <SHUIColorCard style={{ backgroundColor: "#4285F4" }}>
             <div id="card1">
                 <p className="big-text">Placeholder</p>
             </div>
---
+++ src/member.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Row, Col } from 'antd';
+import 'antd/dist/antd.css';
+import "/public/stylesheets/member.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faLinkedin);
+
+function SHUIMember(props) {
+    return (
+        <Row gutter={[0, 0]}>
+            <Col className="gutter-row" span={5}>
+                <img src={props.image} />
+            </Col>
+            <Col className="gutter-row" span={19}>
+                <p className="medium-text">{props.name_title}   <a href={props.linkedin}><FontAwesomeIcon icon={faLinkedin} color="white" /></a></p>
+                <p className="small-text">{props.bio}</p>
+            </Col>
+        </Row>
+    )
+}
+
+export default SHUIMember;
