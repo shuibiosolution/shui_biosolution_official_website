06ed4d9dd49c1647321e9200ae35c8d5fb7c69cd
Yueming <yl3436@cornell.edu>
2021-07-09 23:17:00 -0400

    Changed title images and title styling

--- src/carousel.jsx
+++ src/carousel.jsx
@@ -6,20 +6,20 @@ import "/public/stylesheets/carousel.css";
 function SHUICarousel() {
     return (
         <div className="carousel">
-        <Carousel autoplay dotPosition="bottom">
-            <div class="carousel-container">
-                <img src="/title1.png" className="carousel-image"/>
-                <div><img src="/title4.png" className="carousel-text" /></div>
-            </div >
-            <div class="carousel-container">
-                <img src="/title2.png" className="carousel-image"/>
-                <div><img src="/title4.png" className="carousel-text" /></div>
-            </div>
-            <div class="carousel-container">
-                <img src="/title3.png" className="carousel-image"/>
-                <div><img src="/title4.png" className="carousel-text" /></div>
-            </div>
-        </Carousel>
+            <Carousel autoplay dotPosition="bottom">
+                <div class="carousel-container">
+                    <img src="/title1.png" className="carousel-image" />
+                    <div><img src="/title4.png" className="carousel-text" /></div>
+                </div >
+                <div class="carousel-container">
+                    <img src="/title2.png" className="carousel-image" />
+                    <div><img src="/title5.png" className="carousel-text" /></div>
+                </div>
+                <div class="carousel-container">
+                    <img src="/title3.png" className="carousel-image" />
+                    <div><img src="/title4.png" className="carousel-text" /></div>
+                </div>
+            </Carousel>
         </div>
     );
 }
