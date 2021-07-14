0301c83468453c17f03a7545d84baee44f21da1d
Li Fengyu <fengyuli2002@gmail.com>
2021-07-14 22:48:23 +0800

    Add mongoose.

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const uri = process.env.MONGO_URI || 'mongodb://localhost/test';
+
+mongoose.connect(uri, function(err, res) {
+  
+});
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public/images'));
 app.use(express.static(__dirname + '/build'));
@@ -25,13 +31,16 @@ app.get('/about', (req, res) => {
   res.sendFile(__dirname + "/build/about.min.html");
 })
 
+const now = new Date();
 let postBody = [];
 
 app.post("/contact", (req, res) => {
-  console.log("Post:" + req.body);
   for (const item in req.body) {
     postBody.push(req.body[item]);
   }
+  var date = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate();
+  var time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+  postBody.push(date + " " + time)
   console.log(postBody)
 })
 
