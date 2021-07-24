3f7eb40d7c60b73bfeb3a21f089e7e02d6922d26
Yueming <yl3436@cornell.edu>
2021-07-24 00:36:34 -0400

    Add just all fonts and sizes

--- src/sponsor.jsx
+++ src/sponsor.jsx
@@ -12,11 +12,11 @@ function SHUISponsor(props) {
     return (
         <Row gutter={[0, 0]}>
             <Col className="gutter-row" span={6}>
-                <img src={props.image} className="sponsor-image"/><br />
-                <a href={props.linkedin} target="_blank"><FontAwesomeIcon icon={faLinkedin} color="white" /></a>
-                <a href={props.website} target="_blank"><img src="site.png" width="25%" /></a>
+                <img src={props.image} className="sponsor-image" /><br />
+                <a href={props.linkedin} target="_blank"> <img src="/linkedin.png" id="linkedin" /> </a>
+                <a href={props.website} target="_blank"><img src="site.png" id="site" /></a>
             </Col>
-            <Col className="gutter-row" span={16}>
+            <Col className="gutter-row" span={18}>
                 <p className="medium-text">{props.name}</p>
                 <p className="small-text">{props.intro1}</p>
                 <p className="small-text">{props.intro2}</p>
