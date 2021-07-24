f934cbd30fce39bfc2c07bf5f13268ce78547782
Yueming <yl3436@cornell.edu>
2021-07-24 01:36:32 -0400

    Index page translate

---
+++ src/index.en.jsx
@@ -0,0 +1,44 @@
+/*
+Creating a Pollution-Free World
+
+Our belief is that biotechnology holds the key to many environmental challenges, and that nature has great self-healing abilities.
+
+We are a synthetic biotechnology company incorporated in 2020 focussed on using biosynthesis and computer science to develop new technologies against environmental pollution.
+
+Cutting-edge synthetic materials coupled with innovative bioengineering can provide a revolutionary solution to environmental challenges.
+
+Our Mission
+Protecting Environmental and Human Health
+
+Soil Treatment
+Engineer soil microorganisms to build a integrated system for pollution detection, treatment and facility maintenance
+
+Wastewater Filtration
+Redefine wastewater treatment with low-cost industrial microorganisms
+
+Technology
+
+Synthetic Biology
+Reengineer microbes to endow them with greater functions.
+Combine computer technology and life science for faster development of innovative industrialized microorganisms.
+
+Directed Revolution
+Simulate the process of natural selection to screen the optimal functional proteins.
+
+
+What is the current status of the company?
+With technical support from Bluepha Co., Ltd. and Peking University School of Life Science, SHUI has developed an MVP that effectively tackles heavy metal processing in industrial wastewater filtration. SHUI received an offer from BEWG: Beijing Enterprises Water Group Limited (HKSE: 00371) to test our product in industrial settings and establish corporate partnerships with its subsidiary factories. SHUI raised $15,000 from TechX Academy, $5000 from Cornell Innovation Fund, and $1000 from Penn Weiss Tech Fund and is currently negotiating dilutive funding of $300,000 with MiraclePlus (former YC China). SHUI is currently applying for a patent for its core technology, and the expected first revenue stream will come in mid-May 2021.
+
+
+Do SHUI's products embrace biosafety and environmental safety?
+Absolutely YES, and this part is one of our top strengths. SHUI designed a bio-switch to ensure that functional bacteria will only activate their functionality under prespecified industrial conditions, which ensures biosafety and prevents information leakage. Moreover, SHUI's bio-films are highly biodegradable. At the end of the lifecycle, they will undergo a high temperature and ultraviolet process to make sure the end remnants will possess no negative impact on the environment.
+
+
+What market and customer segments does SHUI mainly target?
+SHUI targets the industrial wastewater treatment market in China, which sizes up to 14.4 billion USD and takes up 26.2% of the global market share. 25.2 billion tons of wastewater is processed in 2019 by 72592 units of treatment facilities. SHUI will mainly serve two types of clients: industrial parks and independent factories. For industrial parks, SHUI replaces multi-step filtration systems with an integrated multi-layer bio-film as one compatible step. For independent factories, SHUI manufactures highly customized biofilms to filter targeted heavy metal pollutants.
+
+
+What is the next step of SHUI?
+SHUI plans to launch a series of promotional campaigns and product demo projects on professional wastewater solution websites, including China’s Water Website and Northern Star Water Treatment Website. SHUI also plans to establish business cooperations with market leaders in industrial wastewater treatment, including Beijing Originwater Technology, Co., Ltd. (SZSE: 300070) and Beijing Enterprises Water Group Limited (HKSE: 00371) with direct corporate outreach. In the larger picture, SHUI plans to build product credibility and elevate brand reputation through successful industrial trials with the aforementioned leading wastewater treatment companies.
+
+*/
