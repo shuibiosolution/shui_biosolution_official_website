abde18c4795800035def53bba4c6906b23db6d9c
Li Fengyu <fengyuli2002@gmail.com>
2021-07-28 09:17:55 +0800

    Test commit to Heroku.

--- src/contact.jsx
+++ src/contact.jsx
@@ -4,7 +4,8 @@ import 'antd/dist/antd.css';
 import "/public/stylesheets/contact.css"
 import axios from "axios";
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = 3000;
 
 function StatusMessage(status) {
     switch (status) {
